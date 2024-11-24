4c7985e5ce334329e988a6378a49b8ad83763b5e
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-11-24 13:30:50 +0300

    feat: add others files

---
+++ src/test/java/com/bookclients/bookingclients_service/BookingClientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookclients.bookingclients_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingClientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
